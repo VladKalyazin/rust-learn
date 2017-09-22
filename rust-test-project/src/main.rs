c4a49867b451989a280fbbbe04ee3a6ccdabfc51
VladKalyazin <vladislav.kalyazin@gmail.com>
2017-09-22 23:07:09 +0300

    Guess number.

---
+++ rust-test-project/src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Write guess number");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Can not read string.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(result) => result,
+            Err(_) => continue
+        };
+
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess number is less."),
+            Ordering::Greater => println!("Your guess number is greater."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
