3710c8b6c9ca034336f045abdce6f869fb079505
Vladislav Kalyazin <vladislav.kalyazin@gmail.com>
2017-10-18 20:14:11 +0300

    Phrases. Test project.

---
+++ phrases/src/japanese/farewells.rs
@@ -0,0 +1,3 @@
+pub fn goodbye() -> String {
+    "片仮名".to_string()
+}
---
+++ test_project/src/main.rs
@@ -0,0 +1,17 @@
+fn merge_sort(source: Vec<i32>) -> Vec<i32> {
+    unimplemented!()
+}
+
+fn merge<'a>(left: &'a Vec<i32>, right: &'a Vec<i32>) -> &'a Vec<i32> {
+    unimplemented!()
+}
+
+fn main() {
+    let vec = vec![4, 5, 1, 6, 9, 2, 5, 3];
+
+    let a = 2;
+    let b = 4;
+    let x = &vec[a..b];
+
+    println!("{:?}", x);
+}
